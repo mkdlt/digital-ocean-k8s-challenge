da0e2b7501d18a8d18c3fa4988c74ac490db7223
mkdlt <mkdltorre@gmail.com>
2021-12-09 09:52:08 +0800

    Add k8s resources to main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,42 @@ terraform {
   }
 }
 
-resource "digitalocean_project" "digital-ocean-k8s-challenge" {
+resource "digitalocean_project" "digital_ocean_k8s_challenge" {
   name        = "digital-ocean-k8s-challenge"
-  description = "DigitalOcean Kubernetes Challenge"
-  purpose     = "Deploy a scalable SQL cluster"
+  description = "Entry for the DigitalOcean Kubernetes Challenge"
+  purpose     = "Just trying out DigitalOcean"
+  environment = "Development"
+
+  resources = [
+    digitalocean_kubernetes_cluster.postgres.urn
+  ]
+}
+
+resource "digitalocean_vpc" "k8s" {
+  name   = "k8s-vpc"
+  region = "sgp1"
+}
+
+data "digitalocean_kubernetes_versions" "prefix" {
+  version_prefix = "1.21."
+}
+
+resource "digitalocean_kubernetes_cluster" "postgres" {
+  name         = "postgres"
+  region       = "sgp1"
+  auto_upgrade = true
+  version      = data.digitalocean_kubernetes_versions.prefix.latest_version
+
+  vpc_uuid = digitalocean_vpc.k8s.id
+
+  maintenance_policy {
+    start_time = "04:00"
+    day        = "sunday"
+  }
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
+  }
 }
