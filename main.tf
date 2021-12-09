1a3976111c91ae79ecb018dd114354e5bce3c8e1
mkdlt <mkdltorre@gmail.com>
2021-12-09 08:33:08 +0800

    Add main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "remote" {
+    organization = "mkdlt"
+
+    workspaces {
+      name = "digital-ocean-k8s-challenge"
+    }
+  }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.16.0"
+    }
+  }
+}
+
+variable "do_token" {}
+variable "pvt_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+data "digitalocean_ssh_key" "terraform-cloud-token" {
+  name = "terraform-cloud-token"
+}
+
+resource "digitalocean_project" "digital-ocean-k8s-challenge" {
+    name        = "digital-ocean-k8s-challenge"
+    description = "DigitalOcean Kubernetes Challenge"
+    purpose     = "Deploy a scalable SQL cluster"
+}
