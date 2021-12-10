2fe717e1d4c905526ff6ec12f4442700fe81ef07
mkdlt <mkdltorre@gmail.com>
2021-12-10 19:49:30 +0800

    Add kubegres manifests

--- main.tf
+++ main.tf
@@ -11,7 +11,60 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.16.0"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.6.0"
+    }
+
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
+}
+
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.postgres.endpoint
+  token = digitalocean_kubernetes_cluster.postgres.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.postgres.kube_config[0].cluster_ca_certificate
+  )
+}
+
+provider "kubectl" {
+  host  = digitalocean_kubernetes_cluster.postgres.endpoint
+  token = digitalocean_kubernetes_cluster.postgres.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.postgres.kube_config[0].cluster_ca_certificate
+  )
+}
+
+data "kubectl_filename_list" "manifest_list" {
+  pattern = "./manifests/*.yaml"
+}
+
+resource "kubectl_manifest" "manifests" {
+  count     = length(data.kubectl_filename_list.manifest_list.matches)
+  yaml_body = file(element(data.kubectl_filename_list.manifest_list.matches, count.index))
+}
+
+variable superUserPassword {}
+variable replicationUserPassword {}
+
+
+resource "kubernetes_secret" "postgres_secret" {
+  metadata {
+    name      = "mypostgres-secret"
+    namespace = "default"
+  }
+
+  data = {
+    superUserPassword       = var.superUserPassword
+    replicationUserPassword = var.replicationUserPassword
+  }
+
+  type = "Opaque"
 }
 
 resource "digitalocean_project" "digital_ocean_k8s_challenge" {
@@ -26,8 +79,12 @@ resource "digitalocean_project" "digital_ocean_k8s_challenge" {
 }
 
 resource "digitalocean_vpc" "k8s" {
-  name   = "k8s-vpc"
-  region = "sgp1"
+  name    = "k8s-vpc"
+  region  = "sgp1"
+  
+  timeouts {
+    delete = "2m"
+  }
 }
 
 data "digitalocean_kubernetes_versions" "prefix" {
