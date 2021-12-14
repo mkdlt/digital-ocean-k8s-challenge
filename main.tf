6c725355edb7853d232ba8855a1964740b66e37d
mkdlt <mkdltorre@gmail.com>
2021-12-14 21:15:34 +0800

    Reorder main.tf

--- main.tf
+++ main.tf
@@ -24,6 +24,50 @@ terraform {
   }
 }
 
+resource "digitalocean_project" "k8s_challenge" {
+  name        = "k8s-challenge"
+  description = "Entry for the DigitalOcean Kubernetes Challenge"
+  purpose     = "Just trying out DigitalOcean"
+  environment = "Development"
+
+  resources = [
+    digitalocean_kubernetes_cluster.postgres.urn
+  ]
+}
+
+resource "digitalocean_vpc" "k8s" {
+  name   = "k8s-vpc"
+  region = "sgp1"
+
+  timeouts {
+    delete = "4m"
+  }
+}
+
+data "digitalocean_kubernetes_versions" "prefix" {
+  version_prefix = "1.21."
+}
+
+resource "digitalocean_kubernetes_cluster" "postgres" {
+  name         = "postgres"
+  region       = "sgp1"
+  auto_upgrade = true
+  version      = data.digitalocean_kubernetes_versions.prefix.latest_version
+
+  vpc_uuid = digitalocean_vpc.k8s.id
+
+  maintenance_policy {
+    start_time = "04:00"
+    day        = "sunday"
+  }
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-1vcpu-2gb"
+    node_count = 3
+  }
+}
+
 provider "kubernetes" {
   host  = digitalocean_kubernetes_cluster.postgres.endpoint
   token = digitalocean_kubernetes_cluster.postgres.kube_config[0].token
@@ -66,48 +110,4 @@ data "kubectl_path_documents" "docs" {
 resource "kubectl_manifest" "kubegres" {
   for_each  = toset(data.kubectl_path_documents.docs.documents)
   yaml_body = each.value
-}
-
-resource "digitalocean_project" "digital_ocean_k8s_challenge" {
-  name        = "digital-ocean-k8s-challenge"
-  description = "Entry for the DigitalOcean Kubernetes Challenge"
-  purpose     = "Just trying out DigitalOcean"
-  environment = "Development"
-
-  resources = [
-    digitalocean_kubernetes_cluster.postgres.urn
-  ]
-}
-
-resource "digitalocean_vpc" "k8s" {
-  name   = "k8s-vpc"
-  region = "sgp1"
-
-  timeouts {
-    delete = "4m"
-  }
-}
-
-data "digitalocean_kubernetes_versions" "prefix" {
-  version_prefix = "1.21."
-}
-
-resource "digitalocean_kubernetes_cluster" "postgres" {
-  name         = "postgres"
-  region       = "sgp1"
-  auto_upgrade = true
-  version      = data.digitalocean_kubernetes_versions.prefix.latest_version
-
-  vpc_uuid = digitalocean_vpc.k8s.id
-
-  maintenance_policy {
-    start_time = "04:00"
-    day        = "sunday"
-  }
-
-  node_pool {
-    name       = "worker-pool"
-    size       = "s-1vcpu-2gb"
-    node_count = 3
-  }
 }
